45834f87cf2400bc591f15f3911914a3f013d75e
Akash <akashkurdekar@gmail.com>
2016-03-23 12:10:18 +0000

    two endpoints in fake auth service

---
+++ fake-auth-service/main.js
@@ -0,0 +1,32 @@
+const app = require('express')(app);
+const _ = require('lodash');
+global.Promise = require('bluebird');
+
+const sessionTokens = [];
+
+// http://localhost:4567/authenticate?username=akash&passcode=boo
+// will fail is username begins with z
+app.get('/authenticate', (req, res) => {
+  const [username, password] = [req.query.username, req.query.password];
+  console.log(`/authenticate called with username ${username} and password ${password}`);
+  const sessionToken = `session_${_.random(1, 10000)}`;
+  sessionTokens.push(sessionToken);
+  res.json({
+    userId: `user_${username}`,
+    sessionToken: sessionToken
+  });
+});
+
+app.get('/session/keep-alive', (req, res) => {
+  console.log('sessionTokens', sessionTokens);
+  if (sessionTokens.indexOf(req.query.sessionToken) === -1) {
+    return res.sendStatus(401);
+  }
+  res.json({
+    sessionToken: req.query.sessionToken
+  });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Fake Auth Service started on port ${process.env.PORT}`);
+});
